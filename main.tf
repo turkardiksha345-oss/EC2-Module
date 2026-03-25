80073f4d795d5719dd8551abed23cf671ec06312
Diksha Turkar <turkardiksha345@gmail.com>
2026-03-25 18:50:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,8 @@
-
-
-resource "aws_security_group" "my_sg" {
-  name        = "my-sg"
-  description = "Allow inbound and outbound rule"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"   
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "my-sg"
-  }
-}
-
 resource "aws_instance" "my_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name   
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  vpc_security_group_ids = var.security_group_id
 
   user_data = <<-EOF
               #!/bin/bash
