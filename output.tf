c6dbca7b3fa345b285545937abe441912fa02c42
Diksha Turkar <turkardiksha345@gmail.com>
2026-03-25 17:57:34 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "instance_id" {
   value = aws_instance.my_instance.id
 }
+
+output "instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
+
+output "security_group_id" {
+  value = aws_security_group.my_sg.id
+}
